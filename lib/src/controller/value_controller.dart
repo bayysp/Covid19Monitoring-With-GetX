695e3a34e1b3abbdbb5591d6fa870165139b8ee3
Bayu Saputra <bayu.saputra@jakpat.net>
2020-08-25 17:32:33 +0700

    remove unused file

--- lib/src/controller/value_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:get/state_manager.dart';
-
-class ValueController extends GetxController{
-  var count = 0.obs;
-  increment() => count.value++;
-}
--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getxexample/src/controller/bindings/sample_bind.dart';
-import 'package:getxexample/src/controller/value_controller.dart';
 import 'package:getxexample/src/view/detail/detail_country_screen.dart';
 import 'package:getxexample/src/view/home/home_page.dart';
 
@@ -10,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -28,32 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-//class MyHomePage extends StatelessWidget {
-//  final ValueController valueController = Get.put(ValueController());
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("title"),
-//      ),
-//      body: Center(
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            Obx(() => Text("Value : " + valueController.count.string)),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: valueController.increment,
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ),
-//    );
-//  }
-//}
