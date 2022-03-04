883c1cb1266d6f34f13bee806c6bbb6adbaf61a3
Bayu Saputra <babahayoo@gmail.com>
2022-03-04 20:17:08 +0700

    :rocket: BREAKING CHANGES, MIGRATING TO NULL SAFETY :sparkles:

--- lib/src/model/country_entity.dart
+++ lib/src/model/country_entity.dart
@@ -1,11 +1,44 @@
-import 'package:getxexample/generated/json/base/json_convert_content.dart';
+class CountryEntity {
+	List<Countries>? countries;
 
-class CountryEntity with JsonConvert<CountryEntity> {
-	List<CountryCountry> countries;
+	CountryEntity({this.countries});
+
+	CountryEntity.fromJson(Map<String, dynamic> json) {
+		if (json['countries'] != null) {
+			countries = <Countries>[];
+			json['countries'].forEach((v) {
+				countries!.add(new Countries.fromJson(v));
+			});
+		}
+	}
+
+	Map<String, dynamic> toJson() {
+		final Map<String, dynamic> data = new Map<String, dynamic>();
+		if (this.countries != null) {
+			data['countries'] = this.countries!.map((v) => v.toJson()).toList();
+		}
+		return data;
+	}
 }
 
-class CountryCountry with JsonConvert<CountryCountry> {
-	String name;
-	String iso2;
-	String iso3;
+class Countries {
+	String? name;
+	String? iso2;
+	String? iso3;
+
+	Countries({this.name, this.iso2, this.iso3});
+
+	Countries.fromJson(Map<String, dynamic> json) {
+		name = json['name'];
+		iso2 = json['iso2'];
+		iso3 = json['iso3'];
+	}
+
+	Map<String, dynamic> toJson() {
+		final Map<String, dynamic> data = new Map<String, dynamic>();
+		data['name'] = this.name;
+		data['iso2'] = this.iso2;
+		data['iso3'] = this.iso3;
+		return data;
+	}
 }
--- lib/src/model/detail_country_entity.dart
+++ lib/src/model/detail_country_entity.dart
@@ -1,23 +1,55 @@
-import 'package:getxexample/generated/json/base/json_convert_content.dart';
+class DetailCountryEntity {
+	Confirmed? confirmed;
+	Confirmed? recovered;
+	Confirmed? deaths;
+	String? lastUpdate;
 
-class DetailCountryEntity with JsonConvert<DetailCountryEntity> {
-	DetailCountryConfirmed confirmed;
-	DetailCountryRecovered recovered;
-	DetailCountryDeaths deaths;
-	String lastUpdate;
+	DetailCountryEntity(
+			{this.confirmed, this.recovered, this.deaths, this.lastUpdate});
+
+	DetailCountryEntity.fromJson(Map<String, dynamic> json) {
+		confirmed = json['confirmed'] != null
+				? new Confirmed.fromJson(json['confirmed'])
+				: null;
+		recovered = json['recovered'] != null
+				? new Confirmed.fromJson(json['recovered'])
+				: null;
+		deaths =
+		json['deaths'] != null ? new Confirmed.fromJson(json['deaths']) : null;
+		lastUpdate = json['lastUpdate'];
+	}
+
+	Map<String, dynamic> toJson() {
+		final Map<String, dynamic> data = new Map<String, dynamic>();
+		if (this.confirmed != null) {
+			data['confirmed'] = this.confirmed!.toJson();
+		}
+		if (this.recovered != null) {
+			data['recovered'] = this.recovered!.toJson();
+		}
+		if (this.deaths != null) {
+			data['deaths'] = this.deaths!.toJson();
+		}
+		data['lastUpdate'] = this.lastUpdate;
+		return data;
+	}
 }
 
-class DetailCountryConfirmed with JsonConvert<DetailCountryConfirmed> {
-	int value;
-	String detail;
-}
+class Confirmed {
+	int? value;
+	String? detail;
 
-class DetailCountryRecovered with JsonConvert<DetailCountryRecovered> {
-	int value;
-	String detail;
-}
+	Confirmed({this.value, this.detail});
 
-class DetailCountryDeaths with JsonConvert<DetailCountryDeaths> {
-	int value;
-	String detail;
+	Confirmed.fromJson(Map<String, dynamic> json) {
+		value = json['value'];
+		detail = json['detail'];
+	}
+
+	Map<String, dynamic> toJson() {
+		final Map<String, dynamic> data = new Map<String, dynamic>();
+		data['value'] = this.value;
+		data['detail'] = this.detail;
+		return data;
+	}
 }
